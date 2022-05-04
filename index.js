2b5101bba4991c7e2854b86c1ca510036e20f961
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-04 11:24:58 +0200

    Commit 2

--- index.js
+++ index.js
@@ -3,8 +3,4 @@
 // Defino la función para
 
 
-// Defino los parámetros que va a recibir la función
-function suma ( numero1, numero2 ){
-    // Creo la suma
-    return numero1 + numero2
-}
+// Commit 2
